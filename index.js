836827cb33169ccc5259d6d931eb064de134cc55
neethupath <pathnee222gmail.com>
2023-03-10 09:47:30 -0500

    looping

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(name,event)
+{
+    let arr= [];
+for(let i=0; i < name.length; i++)
+{
+    arr.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+}
+return arr;
+}
+writeCards(["Guadelope", "Ollie", "Aki"], "surprise");
+
+function countDown(count)
+{
+    while(count>=0)
+    console.log(count--);
+}
+    countDown(4);
+
